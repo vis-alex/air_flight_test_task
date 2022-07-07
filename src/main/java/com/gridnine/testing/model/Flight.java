3ca257f67c98f3ce77637d9bfd4117a6edfb9273
vis-alex <usenkoshurik@mail.ru>
2022-07-07 20:04:56 +0300

    Add Flight model

--- src/main/java/com/gridnine/testing/model/Flight.java
+++ src/main/java/com/gridnine/testing/model/Flight.java
@@ -1,4 +1,22 @@
 package com.gridnine.testing.model;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Flight {
+    private final List<Segment> segments;
+
+    Flight(final List<Segment> segments) {
+        this.segments = segments;
+    }
+
+    List<Segment> getSegments() {
+        return segments;
+    }
+
+    @Override
+    public String toString() {
+        return segments.stream().map(Object::toString)
+                .collect(Collectors.joining(" "));
+    }
 }
